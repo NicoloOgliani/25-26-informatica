883ff40c64fa86c3660b8f7d990080b2be137e92
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2025-09-28 10:32:57 +0000

    realloc29_09

--- realloc25_09.c
+++ realloc25_09.c
@@ -1,5 +1,5 @@
 /*scrivere un programma in linguaggio c che:
-1. allochi dinamicamente un array iniziale di n numeri
+1. allochi dinamicamente un array iniziale di n numeri con malloc
 2. inserimento dei valori
 3. stampa array
 4. calcola la somma degli elementi multipli di 3
@@ -8,33 +8,33 @@
 #include <stdlib.h>
 int* creaVett(int _n){
     int* _array1=NULL;
-    _array1=(int*)mallloc(_n*sizeof(int));
+    _array1=(int*)malloc(_n*sizeof(int));
     if(_array1==NULL){
         printf("Errore di allocazione");
         return 1;
     } 
     return _array1;
 }
-int* inserisciValori(int *_array1; int _n){
+int* inserisciValori(int *array1, int _n){
     for(int i=0; i<_n; i++){
         printf("Inserisci un valore: ");
-        scanf("%d", &_array1[i]);
+        scanf("%d", &array1[i]);
     }
-    return _array1;
+    return array1;
 }
-void stampaVett(int *_array1, int _n){
+void stampaVett(int *array1, int _n){
     printf("Hai inserito i seguenti elementi: ");
     for(int i=0; i<_n; i++){
-        printf("%d ", _array1[i]);
+        printf("%d ", array1[i]);
     }
     printf("\n");
 }
-int sommaMultipli(int *_array1, int _n){
+int sommaMultipli(int *array1, int _n){
     int somma=0, cnt=0;
     for(int i=0; i<_n; i++){
-        if(_array1[i]%3==0){
+        if(array1[i]%3==0){
             cnt++;
-            somma=somma+_array[i];
+            somma=somma+array1[i];
         }
 
     }
@@ -49,10 +49,13 @@ int* creanuovoVett(int _n){
     } 
     return _array2;
 }
-void controlloValori(int *_array2; int _n){
+void controlloValori(int *_array2, int _n){
     for(int i=0; i<_n; i++){
         if(_array2[i]%2==0){
-            _array2=(int*)realloc
+            _array2=(int*)realloc(_array2, (_n-1)*sizeof(int));
+            if(_array2==NULL){
+                printf("Errore di allocazione");
+            return 1;
         }
     }
 }
@@ -66,4 +69,8 @@ int main(){
     stanpaVett(array1, n);
     sommaMultipli(array1, n);
     array2=creanuovoVett(n);
+    controlloValori(array2, n);
+    free(array1);
+    free(array2);
+    return 0;
 }
