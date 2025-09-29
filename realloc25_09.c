7f412511d91229d515e5f281083c3608cc22aedf
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2025-09-29 11:45:40 +0000

    ghes

--- realloc25_09.c
+++ realloc25_09.c
@@ -1,80 +1,87 @@
 /*scrivere un programma in linguaggio c che:
-1. allochi dinamicamente un array iniziale di n numeri con malloc
-2. inserimento dei valori
-3. stampa array
-4. calcola la somma degli elementi multipli di 3
-5. creiamo un nuovo array contenente solo valori dispari */
+ 1. allochi dinamicamente un array iniziale di n numeri con malloc 
+ 2. inserimento dei valori 
+ 3. stampa array 
+ 4. calcola la somma degli elementi multipli di 3 
+ 5. creiamo un nuovo array contenente solo valori dispari */
 #include <stdio.h>
 #include <stdlib.h>
+
 int* creaVett(int _n){
-    int* _array1=NULL;
-    _array1=(int*)malloc(_n*sizeof(int));
-    if(_array1==NULL){
-        printf("Errore di allocazione");
-        exit (1);
+    int* _array1 = (int*)malloc(_n * sizeof(int));
+    if(_array1 == NULL){
+        printf("Errore di allocazione\n");
+        exit(1);
     } 
     return _array1;
 }
-int* inserisciValori(int *_array1, int _n){
-    for(int i=0; i<_n; i++){
-        printf("Inserisci un valore: ");
+
+void inserisciValori(int *_array1, int _n){
+    for(int i = 0; i < _n; i++){
+        printf("Inserisci un valore [%d]: ", i + 1);
         scanf("%d", &_array1[i]);
     }
-    return _array1;
 }
+
 void stampaVett(int *_array1, int _n){
-    printf("Hai inserito i seguenti elementi: ");
-    for(int i=0; i<_n; i++){
+    printf("Hai inserito i seguenti elementi:\n");
+    for(int i = 0; i < _n; i++){
         printf("%d ", _array1[i]);
     }
     printf("\n");
 }
-int sommaMultipli(int *_array1, int _n){
-    int somma=0, cnt=0;
-    for(int i=0; i<_n; i++){
-        if(_array1[i]%3==0){
-            cnt++;
-            somma=somma+_array1[i];
-        }
 
-    }
-    printf("Gli elementi multipli di 3 sono %d e la loro somma è %d", cnt, somma);
-}
-int* creanuovoVett(int _n){
-    int* _array2=NULL;
-    _array2=(int*)malloc(_n*sizeof(int));
-    if(_array2==NULL){
-        printf("Errore di allocazione");
-        exit (1);
-    } 
-    return _array2;
-}
-void controlloValori(int *_array2, int _n){
-    for(int i=0; i<_n; i++){
-        if(_array2[i]%2==0){
-            _array2=(int*)realloc(_array2, (_n-1)*sizeof(int));
-            if(_array2==NULL){
-                printf("Errore di allocazione");
-            exit (1);
-            }
-        }
-        else{
-            printf("%d ", _array2[i]);
+void sommaMultipli(int *_array1, int _n){
+    int somma = 0, cnt = 0;
+    for(int i = 0; i < _n; i++){
+        if(_array1[i] % 3 == 0){
+            cnt++;
+            somma += _array1[i];
         }
     }
+    printf("Gli elementi multipli di 3 sono %d e la loro somma è %d\n", cnt, somma);
 }
+
+int* creaVettDispari(int *_array1,int _sizedispari, int _n){
+    int *_sizeDispari=0;
+    int *_dispari=NULL;
+    int j = 0;
+    for(int i = 0; i < _n; i++){
+        if(_array1[i] % 2 != 0){
+            (*_sizeDispari)++;
+            *_dispari[j++] = _array1[i];
+        }
+    }
+
+    return _dispari;
+}
+
+void stampaArrayDispari(int* array, int size) {
+    printf("Valori dispari:\n");
+    for(int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
 int main(){
-    int *array1=NULL;
-    int *array2=NULL;
-    int n=0;
-    
-    array1=creaVett(n);
-    array1=inserisciValori(array1, n);
+    int *array1 = NULL;
+    int n = 0;
+    int *dispari = NULL;
+    int *sizedispari=0;
+
+    printf("Quanti numeri vuoi inserire? ");
+    scanf("%d", &n);
+
+    array1 = creaVett(n);
+    inserisciValori(array1, n);
     stampaVett(array1, n);
     sommaMultipli(array1, n);
-    array2=creanuovoVett(n);
-    controlloValori(array2, n);
+
+    dispari = creaVettDispari(array1, n, dispari);
+    stampaArrayDispari(dispari,&sizedispari);
+
     free(array1);
-    free(array2);
+    free(dispari);
     return 0;
 }
