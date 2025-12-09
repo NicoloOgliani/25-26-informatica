9f9b29d1e69b68cc72f2cc1595e29bdc2a13c85c
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2025-12-09 09:34:24 +0000

    9_12allenatore

--- struct/3es2_12allenatore.c
+++ struct/3es2_12allenatore.c
@@ -1,75 +1,61 @@
 /*creare una struttura squadra con nome
 colore, punteggio e le informazioni del proprio allenatore, 
 (nome, cognome, titoli), poi stampiamo sollo le squadre 
-che hanno un punteggio superiore a 100 e numero di titoli per allenatore superiore a 10. Caricare un max di 10 squadre*/
+che hanno un punteggio superiore a 30 e numero di titoli per allenatore superiore a 10. Caricare un max di 10 squadre, fai con le funzioni*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-    char nome[20];
-    char cognome[20];
-    int titoli;
-} Allenatore;
-
-typedef struct {
+struct squadra{
     char nome[20];
     char colore[20];
     int punteggio;
-    Allenatore coach;
-} Squadra;
-
-void stampa(Squadra *teams, int n) {
-    for (int i = 0; i < n; i++) {
-        if (teams[i].punteggio > 100 && teams[i].coach.titoli > 10) {
-            printf("Nome squadra: %s\n", teams[i].nome);
-            printf("Colore: %s\n", teams[i].colore);
-            printf("Punteggio: %d\n", teams[i].punteggio);
-            printf("Allenatore: %s %s\n", teams[i].coach.nome, teams[i].coach.cognome);
-            printf("Titoli: %d\n\n", teams[i].coach.titoli);
+    struct allenatore{
+        char nomeAllenatore[20];
+        char cognome [20];
+        int titoli;
+    }mister;
+};
+typedef struct squadra squadre;
+void caricaSquadre(squadre *team, int n){
+    for(int i=0; i<n; i++){
+        printf("Inserisci il nome della squadra: ");
+        scanf("%s", team[i].nome);
+        printf("Inserisci il colore della squadra: ");
+        scanf("%s", team[i].colore);
+        printf("Inserisci il punteggio della squadra: ");
+        scanf("%d", &team[i].punteggio);
+        printf("Inserisci il nome dell'allenatore: ");
+        scanf("%s", team[i].mister.nomeAllenatore);
+        printf("Inserisci il cognome dell'allenatore: ");
+        scanf("%s", team[i].mister.cognome);
+        printf("Inserisci il numero di titoli dell'allenatore: ");
+        scanf("%d", &team[i].mister.titoli);
+    }   
+}
+void stampaSquadre(squadre team[], int n){
+    printf("\nSquadre con punteggio superiore a 30 e allenatore con piu' di 10 titoli:\n");
+    for(int i=0; i<n; i++){
+        if(team[i].punteggio > 30 && team[i].mister.titoli > 10){
+            printf("Squadra: %s, Colore: %s, Punteggio: %d, Allenatore: %s %s, Titoli: %d\n",
+                   team[i].nome, team[i].colore, team[i].punteggio,
+                   team[i].mister.nomeAllenatore, team[i].mister.cognome,
+                   team[i].mister.titoli);
         }
     }
 }
-
-void leggi_stringa(char *dest, int size) {
-    fgets(dest, size, stdin);
-    int len = strlen(dest);
-    if (len > 0 && dest[len - 1] == '\n') {
-        dest[len - 1] = '\0';  // rimuove il newline
-    }
-}
-
-int main() {
-    Squadra teams[10];
+int main(){
+    squadre *team;
     int n;
-
-    printf("Quante squadre vuoi inserire? (max 10)\n");
+    //fai una malloc per n squadre
+    team=(squadre *)malloc(10 * sizeof(squadre));
+    do{
+    printf("Quante squadre vuoi inserire (max 10)? ");
     scanf("%d", &n);
-    getchar(); // consuma il newline rimasto da scanf
-    if (n > 10) n = 10;
-
-    for (int i = 0; i < n; i++) {
-        printf("Nome squadra: ");
-        leggi_stringa(teams[i].nome, 20);
-
-        printf("Colore squadra: ");
-        leggi_stringa(teams[i].colore, 20);
-
-        printf("Punteggio: ");
-        scanf("%d", &teams[i].punteggio);
-        getchar();
-
-        printf("Nome allenatore: ");
-        leggi_stringa(teams[i].coach.nome, 20);
-
-        printf("Cognome allenatore: ");
-        leggi_stringa(teams[i].coach.cognome, 20);
-
-        printf("Titoli allenatore: ");
-        scanf("%d", &teams[i].coach.titoli);
-        getchar();
-    }
-
-    stampa(teams, n);
+    }while(n>=10);
+    caricaSquadre(team, n);
+    stampaSquadre(team, n);
     return 0;
-}
+}   
+
+
