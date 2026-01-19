c2a6d04e987d218dd60910a4a61c459d9b99117b
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2026-01-19 21:12:54 +0000

    allenatore e squadre

--- struct/3es2_12allenatore.c
+++ struct/3es2_12allenatore.c
@@ -8,55 +8,77 @@ con punteggio superiore a 100 e numero di titoli per allenatore superiore a 10
 #include <stdlib.h>
 #include <string.h>
 
-struct squadra{
+typedef struct{
     char nome[20];
     char colore[20];
     int punteggio;
     struct allenatore{
-        char nomeAllenatore[20];
-        char cognome [20];
+        char nome[20];
+        char cognome[20];
         int titoli;
     }mister;
-};
-typedef struct squadra squadre;
-void caricaSquadre(squadre *team, int n){
+}Squadra;
+void pulisciBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void rimuoviNewline(char *s){
+    int i=0;
+    while(s[i]!='\0'){
+        if(s[i]=='\n'){
+            s[i]='\0';
+        }
+        i++;
+    }
+}
+void caricaSquadre(Squadra *team, int n){
     for(int i=0; i<n; i++){
         printf("Inserisci il nome della squadra: ");
-        scanf("%s", team[i].nome);
+        fgets(team[i].nome, 20, stdin);
+        rimuoviNewline(team[i].nome);
+
         printf("Inserisci il colore della squadra: ");
-        scanf("%s", team[i].colore);
+        fgets(team[i].colore, 20, stdin);
+        rimuoviNewline(team[i].colore);
+
         printf("Inserisci il punteggio della squadra: ");
         scanf("%d", &team[i].punteggio);
+        pulisciBuffer();
+        
         printf("Inserisci il nome dell'allenatore: ");
-        scanf("%s", team[i].mister.nomeAllenatore);
+        fgets(team[i].mister.nome, 20, stdin);
+        rimuoviNewline(team[i].mister.nome);
+
         printf("Inserisci il cognome dell'allenatore: ");
-        scanf("%s", team[i].mister.cognome);
-        printf("Inserisci il numero di titoli dell'allenatore: ");
+        fgets(team[i].mister.cognome, 20, stdin);
+        rimuoviNewline(team[i].mister.cognome);
+
+        printf("Inserisci i titoli dell'allenatore: ");
         scanf("%d", &team[i].mister.titoli);
-    }   
+        pulisciBuffer();
+
+    }
 }
-void stampaSquadre(squadre team[], int n){
-    printf("\nSquadre con punteggio superiore a 30 e allenatore con piu' di 10 titoli:\n");
+void stampaSquadre(Squadra *team, int n){
     for(int i=0; i<n; i++){
-        if(team[i].punteggio > 30 && team[i].mister.titoli > 10){
-            printf("Squadra: %s, Colore: %s, Punteggio: %d, Allenatore: %s %s, Titoli: %d\n",
-                   team[i].nome, team[i].colore, team[i].punteggio,
-                   team[i].mister.nomeAllenatore, team[i].mister.cognome,
-                   team[i].mister.titoli);
+        if(team[i].punteggio>100 && team[i].mister.titoli>10){
+            printf("La squadra: %s ha fatto più di 100 punti e vinto + di 10 titoli", team[i].nome);
         }
     }
 }
 int main(){
-    squadre *team;
-    int n;
-    team=(squadre *)malloc(10 * sizeof(squadre));
+    Squadra *team;
+    int n=0;
+    team=(Squadra*)malloc(10*sizeof(Squadra));
     do{
-    printf("Quante squadre vuoi inserire (max 10)? ");
-    scanf("%d", &n);
+        printf("Quante squadre vuoi inserire: ");
+        scanf("%d", &n);
+        pulisciBuffer();
     }while(n>=10);
     caricaSquadre(team, n);
     stampaSquadre(team, n);
+    free(team);
     return 0;
-}   
-
+}
 
