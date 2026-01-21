67119ee728eb40461cdf2eabb889f1ddc6d26849
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2026-01-21 17:02:55 +0000

    ripasso tpsi

---
+++ struct/6libri.c
@@ -0,0 +1,146 @@
+/* Registarte le informazioni di alcuni Libri ad esempio 
+	titolo, autore, anno e prezzo, determinare il libro 
+	più costoso e il più vecchio. 
+	Eliminare un libro
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct LIBRO{
+	char titolo[40];
+	char autore[20];
+	int anno;
+	float prezzo;
+};
+typedef char* Stringa;
+void Compattamento(Stringa buffer, int lunghezza){				
+	
+    if(lunghezza>0 && buffer[lunghezza-1]=='\n'){
+        buffer[lunghezza-1] = '\0';
+        lunghezza--;
+    }
+}
+
+int main(){
+	struct LIBRO *libri;
+	char libro[40];
+	int i, j, n=3, datato, i_costoso, i_datato, lun;
+	float costoso;
+	
+	//allocazione dei primi 3 elementi
+	libri = (struct LIBRO*)malloc(n * sizeof(struct LIBRO));
+    
+    if (libri == NULL) {
+        printf("Errore: allocazione memoria fallita\n");
+        return 1;
+    }
+    
+    //inserimento dei dati
+	strcpy(libri[0].titolo, "L'amore mio non muore");
+	strcpy(libri[0].autore, "Roberto Saviano");
+	libri[0].anno = 2025;
+	libri[0].prezzo = 19.5;
+
+	strcpy(libri[1].titolo, "La tua voce e\' un drago alato");
+	strcpy(libri[1].autore, "Alessandra Battaglia");
+	libri[1].anno = 2024;
+	libri[1].prezzo = 18.5;
+	
+	strcpy(libri[2].titolo, "Il diritto di contare");
+	strcpy(libri[2].autore, "Margot Lee Shetterly");
+	libri[2].anno = 2017;
+	libri[2].prezzo = 14;
+	/*
+	//visualizzazione dati
+	printf("=== Lista Libri ===\n");
+    for (i = 0; i < n; i++) {
+        printf("Libro %d:\n", i + 1);
+        printf("  Titolo: %s\n", libri[i].titolo);
+        printf("  Autore: %s\n", libri[i].autore);
+        printf("  Anno: %d anni\n", libri[i].anno);
+        printf("  Prezzo: euro %.2f\n\n", libri[i].prezzo);
+    }*/
+    
+    //supponiamo di voler aggiungere un libro
+    //reallochiamo a 4 elementi
+    n += 1;
+    libri = (struct LIBRO*)realloc(libri, n * sizeof(struct LIBRO));
+    
+    if (libri == NULL) {
+        printf("Errore: ridimensionamento fallito\n");
+        return 1;
+    }
+	
+	strcpy(libri[3].titolo, "Orgoglio e pregiudizio");
+	strcpy(libri[3].autore, "Jane Austen");
+	libri[3].anno = 1813;
+	libri[3].prezzo = 9;
+	
+	//Visualizziamo l'Array
+    for (i = 0; i < n; i++) {
+        printf("Libro %d:\n", i + 1);
+        printf("  Titolo: %s\n", libri[i].titolo);
+        printf("  Autore: %s\n", libri[i].autore);
+        printf("  Anno: %d anni\n", libri[i].anno);
+        printf("  Prezzo: euro %.2f\n\n", libri[i].prezzo);
+    }
+    
+    //cerchiamo quello più costoso
+    costoso = libri[0].prezzo;
+    i_costoso = 0;
+    for (i = 1; i < n; i++) {
+        if( libri[i].prezzo > costoso){
+        	costoso = libri[i].prezzo;
+			i_costoso = i;	
+		}  	
+    }
+    printf("Il libro piu\' costoso e\': %s\n", libri[i_costoso].titolo);
+	
+	//cerchiamo quello più vecchio
+	datato = libri[0].anno;
+    i_datato = 0;
+    for (i = 1; i < n; i++) {
+        if( libri[i].anno < datato){
+        	datato = libri[i].anno;
+			i_datato = i;	
+		}  	
+    }
+    printf("Il libro piu\' datato e\': %s\n", libri[i_datato].titolo);
+    
+    //eliminiamo un libro 
+    printf("\nInserisci il tiolo del libro da eliminare: ");		
+	fgets(libro, 50, stdin); 
+	lun = strlen(libro);
+	Compattamento(libro, lun);
+	for (i = 0; i < n; i++) {
+		if( !(strcmp(libri[i].titolo,libro)) ){
+			for(j=i; j<(n-1); j++){
+				strcpy(libri[j].titolo,libri[j+1].titolo);
+				strcpy(libri[j].autore,libri[j+1].autore);
+				libri[j].anno = libri[j+1].anno;
+				libri[j].prezzo = libri[j+1].prezzo;
+			}
+			n-=1;	
+		}  	
+    }
+    
+    //reallochiamo il nuovo array 
+    libri = (struct LIBRO*)realloc(libri, n * sizeof(struct LIBRO));
+    
+    if (libri == NULL) {
+        printf("Errore: ridimensionamento fallito\n");
+        return 1;
+    }
+    //Visualizziamo l'Array
+    for (i = 0; i < n; i++) {
+        printf("Libro %d:\n", i + 1);
+        printf("  Titolo: %s\n", libri[i].titolo);
+        printf("  Autore: %s\n", libri[i].autore);
+        printf("  Anno: %d anni\n", libri[i].anno);
+        printf("  Prezzo: euro %.2f\n\n", libri[i].prezzo);
+    }
+	
+	free(libri);
+	libri=NULL;
+}
--- struct/7correzStudenti.c
+++ struct/7correzStudenti.c
@@ -3,71 +3,124 @@ cognome, nome, classe, maticola, n_certifi
 e le informazioni riguardo le certificazioni di ogni studente:
 matricola, esame(corso, livello) */
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-typedef struct Studente{
-    char cognome[20];
-    char nome[20];
-    char classe[3];
-    int matricola;
-    int n_certif;
-}Studente;
-typedef struct Certificazione{
-    int matricola;
-    struct esame{
-        char corso[20];
-        int livello;
-    }esame;
-}Certificazione;
-int main(){
-    Studente *studenti;
-    Certificazione *certificazioni;
-    int n=3, num, c_max, i_max;
+#include <stdlib.h>
 
-    certificazioni=(Certificazione*)malloc(n*sizeof(Certificazione));
-    if(certificazioni==NULL){
-        printf("Errore di allocazione");
+struct Studente{
+	char Cognome[20];
+	char Nome[20];
+	char classe[3];
+	int matricola;
+	int n_certif;
+};
+
+struct Certificazione{
+	int matricola;
+	struct esame{
+		char corso[20];
+		int livello;
+	}esame;
+};
+
+int main(){
+	struct Studente *studenti;
+	struct Certificazione *certificazioni;
+	int n, i, j, c_max=0, num=0, i_max;
+	
+	//Allochiamo i primi 2 studenti 
+	n = 3;
+	studenti = (struct Studente*)malloc(n * sizeof(struct Studente));
+    certificazioni = (struct Certificazione*)malloc(n * sizeof (struct Certificazione));
+    if (studenti == NULL) {
+        printf("Errore: allocazione memoria fallita\n");
         return 1;
     }
-    studenti=(Studente*)malloc(n*sizeof(Studente));
-    if(studenti==NULL){
-        printf("Errore di allocazione");
+	if (certificazioni == NULL) {
+        printf("Errore: allocazione memoria fallita\n");
         return 1;
     }
-    //Inserimento dati studente
-    strcpy(studenti[0].nome, "Mario");
-    strcpy(studenti[0].cognome, "rossi");
-    strcpy(studenti[0].classe, "3A");
-    studenti[0].matricola="223";
-    studenti[0].n_certif="0";
-    //Inserimento certificazioni
-    certificazioni[0].matricola="223";
-    strcpy(certificazioni[0].esame.corso, "Inglese");
-    certificazioni[0].esame.livello="2";
-    //stampa studenti
-    for(int i=0; i<n; i++){
-        printf("Studente %d", i+1);
-        printf("Cognome %s:\n", studenti[i].cognome); 
-        printf("Nome %s:\n", studenti[i].nome); 
-        printf("Classe %s:\n", studenti[i].classe); 
-        printf("Matricola %d:\n", studenti[i].matricola);
+
+    //inseriamo gli elementi e stampiamoli
+    strcpy(studenti[0].Cognome, "Rossi");
+	strcpy(studenti[0].Nome, "Giulio");
+	strcpy(studenti[0].classe, "4I");
+	studenti[0].matricola = 123;  
+	studenti[0].n_certif = 0;  
+	
+	strcpy(studenti[1].Cognome, "Verdi");
+	strcpy(studenti[1].Nome, "Carlo");
+	strcpy(studenti[1].classe, "3I");
+	studenti[1].matricola = 456;  
+	studenti[1].n_certif = 0; 
+	
+	strcpy(studenti[2].Cognome, "Gialli");
+	strcpy(studenti[2].Nome, "Sofia");
+	strcpy(studenti[2].classe, "4I");
+	studenti[2].matricola = 789;
+	studenti[2].n_certif = 0; 
+	 
+	//adesso inseriamo in base alle matricole i corsi e il livello
+	certificazioni[0].matricola = 123;
+	strcpy(certificazioni[0].esame.corso,"inglese");
+	certificazioni[0].esame.livello = 3;
+	
+	certificazioni[1].matricola = 123;
+	strcpy(certificazioni[1].esame.corso,"tedesco");
+	certificazioni[1].esame.livello = 1; 
+	
+	certificazioni[2].matricola = 789;
+	strcpy(certificazioni[2].esame.corso,"spagnolo");
+	certificazioni[2].esame.livello = 1;  
+	
+	//visualizziamo gli studenti
+	printf("=== Lista Studenti ===\n");
+    for (i = 0; i < n; i++) {
+        printf("Studente %d:\n", i + 1);
+        printf("  Cognome: %s\n", studenti[i].Cognome);
+        printf("  Nome: %s\n", studenti[i].Nome);
+        printf("  Classe: %s\n", studenti[i].classe);
+        printf("  Matricola:  %d\n", studenti[i].matricola);
+        printf("  Numero Certificazioni:  %d\n\n", studenti[i].n_certif);
     }
-    printf("Corso %d:\n", certificazioni[i].esame.corso); 
-    for(int i=0; i<n; i++){
-        num=studenti[i].matricola;
-        for(int j=0; j<n; j++){
-            if(certificazioni[j].matricola == num){ 
-                studenti[i].n_certif++; 
-            }
-        }
+    //visualizziamo le certificazioni relative alle matricole
+	printf("=== Lista Certificazioni ===\n");
+    for (i = 0; i < n; i++) {
+        printf("  Matricola: %d\n", certificazioni[i].matricola);
+        printf("  Corso: %s\n", certificazioni[i].esame.corso);
+        printf("  Livello: %d\n\n", certificazioni[i].esame.livello);
     }
-    c_max=studenti[0].n_certif;
-    i_max=0;
-    for(int i=0; i<n; i++){ 
-        if(studenti[i].n_certif > c_max){ 
-            c_max = studenti[i].n_certif; 
-            i_max = i; 
-        } 
+    
+    //calcoliamo il numero delle certificazioni di ogni studente
+    for(i=0; i<n; i++){
+    	num = studenti[i].matricola;
+    	for(j=0; j<n; j++){
+    		if(certificazioni[j].matricola == num){
+    			studenti[i].n_certif +=1;
+			}
+		}
+	}
+	
+	//visualizziamo gli studenti
+	printf("=== Lista Studenti ===\n");
+    for (i = 0; i < n; i++) {
+        printf("Studente %d:\n", i + 1);
+        printf("  Cognome: %s\n", studenti[i].Cognome);
+        printf("  Nome: %s\n", studenti[i].Nome);
+        printf("  Classe: %s\n", studenti[i].classe);
+        printf("  Matricola:  %d\n", studenti[i].matricola);
+        printf("  Numero Certificazioni:  %d\n\n", studenti[i].n_certif);
     }
-    printf("Lo studente con il maggior numero di certificazioni è %s %s", studenti[i_max].Cognome, studenti[i_max].Nome);
+	
+	//calcoliamo lo studente con pių certificazioni
+	c_max = studenti[0].n_certif;
+	i_max = 0;
+	for(i=0; i<n; i++){
+    		if(studenti[i].n_certif > c_max){
+    			c_max = studenti[i].n_certif;
+    			i_max = i;
+			}
+	}	
+	printf("Lo studente con piu\' certificazioni e\': %s, %s, %s, matricola: %d",studenti[i_max].Cognome , studenti[i_max].Nome, studenti[i_max].classe, studenti[i_max].matricola);
+	free(studenti);
+	free(certificazioni);
 }
